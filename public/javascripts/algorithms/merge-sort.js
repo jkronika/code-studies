ed6d2492bf631fbfb2d18bd0f969b18f8fe183ec
Jake Kronika <jkronika@gmail.com>
2017-02-05 12:58:00 -0800

    copying rather than shifting in merge sort (better performance)

--- public/javascripts/algorithms/merge-sort.js
+++ public/javascripts/algorithms/merge-sort.js
@@ -1,13 +1,15 @@
 function merge(array, p, q, r) {
     let low = [].concat(array.slice(p, q+1));
     let high = [].concat(array.slice(q+1, r+1));
+    let i = 0;
+    let j = 0;
 
-    while (high.length) {
-        array[p++] = (!low.length || high[0] < low[0]) ? high.shift() : low.shift();
+    while (j < high.length) {
+        array[p++] = (i >= low.length || high[j] < low[i]) ? high[j++] : low[i++];
     }
 
-    while (low.length) {
-        array[p++] = low.shift();
+    while (i < low.length) {
+        array[p++] = low[i++];
     }
 }
 
